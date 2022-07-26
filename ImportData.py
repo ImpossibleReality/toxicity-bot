92e6f3e16c8979c44a638a95a8e8f9e47e3a5e3b
Dhillonr25 <109825795+Dhillonr25@users.noreply.github.com>
2022-07-26 11:22:33 -0700

    Update ImportData.py

--- ImportData.py
+++ ImportData.py
@@ -1,2 +1,32 @@
+# YOU NEED TO DO THE FOLLOWING BEFORE RUNNING THIS:
+# pip install datasets
+# pip install kaggle
+
+
 import pandas as pd
-pd.impor
+from datasets import load_dataset
+
+
+dataset = load_dataset("hate_speech18")
+huggingFaceData1 = pd.read_csv(dataset, usecols=[0,4])
+
+
+
+
+
+kagglaData1 = pd.read_csv("https://www.kaggle.com/datasets/mrmorj/hate-speech-and-offensive-language-dataset/
+
+
+
+
+
+
+
+
+
+
+
+
+#print first 5 lines of the data
+huggingFaceData1.head()
+
