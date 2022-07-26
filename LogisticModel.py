b1feb44dae94fc6ddc04380f3669b7222be653ee
Impossible Reality <impossible@impreality.ml>
2022-07-26 13:07:17 -0700

    :poop: Pls fix

--- LogisticModel.py
+++ LogisticModel.py
@@ -1,73 +1,92 @@
 import pandas as pd
 import numpy as np
+import numpy.typing as npt
 from sklearn.feature_extraction.text import CountVectorizer
-#from numba import jit
+from sklearn.preprocessing import OneHotEncoder
+from tqdm import tqdm
+from numba import njit
+
+
+@njit
+def sigmoid(x, w, b=0):
+    cow = w * (x + b)
+    for i in range(len(cow)):
+        frog = cow[i]
+        if frog < 0:
+            return np.exp(frog) / (1 + np.exp(frog))
+        else:
+            return 1 / (1 + np.exp(-frog))
+
+
+@njit
+def update_parameters(X, Y, w, b, learning_rate: int):
+    n = len(X)
+
+    dl_dw = np.zeros(len(X[0]))
+    dl_db = 0
+
+    # ROHAN'S MATH
+    for i in range(n):
+        dl_dw += X[i] * (Y[i] - sigmoid(X[i], w))
+        dl_db += sigmoid(w, X[i], b) - Y[i]
+
+    w += learning_rate * dl_dw
+    b += learning_rate * dl_db
+
+    return w, b
+
+@njit
+def loss(X, Y, w, b):
+    ls = 0
+    for i in range(X.shape[0]):
+        if Y[i] == 0:
+            ls += np.log(np.subtract(1, sigmoid(X[i], w, b)))
+        else:
+            ls += np.log(sigmoid(X[i], w, b))
+    return ls
+
 
 class LogisticModel:
-    
-    def sigmoid(x, w, b = 0):
-        #return 1 / (np.exp(np.negative(np.add(np.multiply(w, x), b))))
-        return 1 / (np.exp(- w*x + b))
-    
-    def __init__(self, learningRate, epochs, cutoff):
-        self.learningRate = learningRate
+
+    def __init__(self, learning_rate, epochs, cutoff):
+        self.b = 0
+        self.w = np.zeros(1)
+        self.learningRate = learning_rate
         self.epochs = epochs
         self.cutoff = cutoff
-    
-    def train(self, X, Y):
-        
-        self.w = 0
-        self.b = 0
-        
-        for i in range(self.epochs):
-            self.w, self.b = self.updateParameters(X, Y, self.learningRate) 
-        
-        return self
-    
-    #@jit    
-    def updateParameters(self, X, Y, learningRate):
-        
-        w = self.w
-        b = self.b
-                
-               
-        N = len(X)
-        
-        dl_dw = np.zeros(N)
-        dl_db = 0
-        
-        # ROHAN'S MATH
-        for i in range(len(X)):
-            dl_dw[i] += X[i] * (Y[i] - self.sigmoid(X[i], w[i]))
-            dl_db += self.sigmoid(w[i], X[i], b) - Y[i]
-      
-        w += learningRate * dl_dw
-        b += learningRate * dl_db
-        
-        return w, b       
 
-    def loss(self, X, Y, w, b):
-        l = 0
-        for i in range(len(X)):
-            if Y[i] == 0:
-                l += np.log(np.subtract(1, self.sigmoid(X[i], w, b)))
-            else:
-                l += np.log(self.sigmoid(X[i], w, b))
-        return l
-    
-    
+    def train(self, X, Y):
+
+        self.w = np.zeros(len(X[0]))
+
+        for _ in tqdm(range(self.epochs)):
+            self.w, self.b = update_parameters(X, Y, self.w, self.b, self.learningRate)
+        return self
+
     def pred(self, testX):
-        return self.sigmoid(testX, self.w, self.b)
-        
-    
-data = pd.read_csv('../data/cleaned_data.csv')
+        print(testX, self.w, self.b)
+        return sigmoid(testX, self.w, self.b)
+
+
+data = pd.read_csv('data/cleaned_data.csv')
 texts = data['text'].astype(str)
 y = data['class']
 
 # Bag Of Words Model
 # (https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html)
-count_vectorizer = CountVectorizer(stop_words='english', min_df=0.0001)
+
+count_vectorizer = CountVectorizer(stop_words='english', min_df=0.0001, binary=True)
 x = count_vectorizer.fit_transform(texts)
 
+print('Start training', len(count_vectorizer.get_feature_names_out()))
+model = LogisticModel(2, 20, 0.5)
+model.train(x.toarray(), np.array(y))
 
-model = LogisticModel(2, 5000, 0.5)
+x2 = count_vectorizer.transform(["You stupid motherfucker"])
+print(model.pred(x2.toarray()[0]))
+
+x2 = count_vectorizer.transform(["Hello"])
+print(model.pred(x2.toarray()[0]))
+
+x2 = count_vectorizer.transform(["I am stupid"])
+print(model.pred(x2.toarray()[0]))
