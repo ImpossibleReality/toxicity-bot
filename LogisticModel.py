e79405f18b78dc70910fe1f697e9e14608048ae9
Daniel Wang <DanielW25@lakesideschool.org>
2022-07-26 10:52:46 -0700

    Added gradient calculations
    Added partial derivative calculations of the log-loss function of the sigmoid function of the Logistic Regression model

--- LogisticModel.py
+++ LogisticModel.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
-
+from sklearn.feature_extraction.text import CountVectorizer
+#from numba import jit
 
 class LogisticModel:
     
-    def sigmoid(x, w, b):
-        return 1 / (np.exp(np.negative(np.add(np.multiply(w, x), b))))
+    def sigmoid(x, w, b = 0):
+        #return 1 / (np.exp(np.negative(np.add(np.multiply(w, x), b))))
+        return 1 / (np.exp(- w*x + b))
     
     def __init__(self, learningRate, epochs, cutoff):
         self.learningRate = learningRate
@@ -21,17 +23,26 @@ class LogisticModel:
             self.w, self.b = self.updateParameters(X, Y, self.learningRate) 
         
         return self
-        
+    
+    #@jit    
     def updateParameters(self, X, Y, learningRate):
         
         w = self.w
         b = self.b
+                
+               
+        N = len(X)
         
-        pred = self.pred(X)
+        dl_dw = np.zeros(N)
+        dl_db = 0
         
-        #### GRADIENT DESCENT HERE
-        #w: 
-        #b:     
+        # ROHAN'S MATH
+        for i in range(len(X)):
+            dl_dw[i] += X[i] * (Y[i] - self.sigmoid(X[i], w[i]))
+            dl_db += self.sigmoid(w[i], X[i], b) - Y[i]
+      
+        w += learningRate * dl_dw
+        b += learningRate * dl_db
         
         return w, b       
 
@@ -52,3 +63,11 @@ class LogisticModel:
 data = pd.read_csv('../data/cleaned_data.csv')
 texts = data['text'].astype(str)
 y = data['class']
+
+# Bag Of Words Model
+# (https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html)
+count_vectorizer = CountVectorizer(stop_words='english', min_df=0.0001)
+x = count_vectorizer.fit_transform(texts)
+
+
+model = LogisticModel(2, 5000, 0.5)
