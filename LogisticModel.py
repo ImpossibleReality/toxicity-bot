b98189a10c6ed797dbd6fd324d74f5817667f624
Impossible Reality <impossible@impreality.ml>
2022-07-27 12:43:37 -0700

    :bug: Fix Daniels code

--- LogisticModel.py
+++ LogisticModel.py
@@ -1,30 +1,37 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
-#from numba import jit
+from sklearn.model_selection import train_test_split
+from numba import njit
 from tqdm import tqdm
 
 
-#@jit
+#@njit
 def sigmoid(x, w, bias=0):
     # sigmoid(b + x1w1 + x2w2 + ...)
-    r = x.dot(w)
-    return 1 / (1 + np.exp(-r - bias))
+    r = np.dot(x, w)
+    calc = 1 / (1 + np.exp(-r - bias))
+    if calc == 0:
+        return 0.0001
+    if calc == 1:
+        return 0.99999
+    return calc
 
+def get_row(X, i):
+    return X.getrow(i).toarray()
 
-#@jit
+#@njit
 def update_parameters(X, Y, w, b, learning_rate: int):
-    n = X.get_shape()[0]
+    n = X.shape[0]
     
-    dl_dw = np.zeros((X.get_shape()[1], 1))
+    dl_dw = np.zeros((X.shape[1], 1))
     dl_db = 0
     
     # ROHAN'S MATH
-    for i in range(n):
-        xi = X.getrow(i).toarray()
-
-        dl_dw += np.transpose(np.multiply(xi, (sigmoid(xi, w) - Y[i])))
-        dl_db += sigmoid(xi, w, b) - Y[i]
+    i = np.random.randint(0, n, 1)
+    xi = get_row(X, i)
+    dl_dw += np.transpose(np.multiply(xi, (sigmoid(xi, w) - Y[i])))
+    dl_db += sigmoid(xi, w, b) - Y[i]
    
    
     w -= learning_rate * dl_dw
@@ -33,14 +40,14 @@ def update_parameters(X, Y, w, b, learning_rate: int):
     return w, b
 
 
-#@jit
-def loss(X, Y, w, b):
+#@njit
+def _loss(X, Y, w, b):
     ls = 0
     for i in range(X.shape[0]):
         if Y[i] == 0:
-            ls += np.log(np.subtract(1, sigmoid(X[i], w, b)))
+            ls += np.log(np.subtract(1, sigmoid(get_row(X, i), w, b)))
         else:
-            ls += np.log(sigmoid(X[i], w, b))
+            ls += np.log(sigmoid(get_row(X, i), w, b))
     return ls
 
 
@@ -53,10 +60,15 @@ class LogisticModel:
         self.epochs = epochs
         self.cutoff = cutoff
 
+    def loss(self, X, Y):
+        return _loss(X, Y, self.w, self.b)
+
     def train(self, X, Y):
         self.w = np.zeros((X.get_shape()[1], 1))
 
-        for _ in tqdm(range(self.epochs)):
+        for i in tqdm(range(self.epochs)):
+            if i % 100000 == 0:
+                print("Loss:", self.loss(X, Y))
             self.w, self.b = update_parameters(X, Y, self.w, self.b, self.learningRate)
         
         return self
@@ -68,29 +80,35 @@ class LogisticModel:
 
 
 data = pd.read_csv('data/cleaned_data.csv')
-texts = data['text'].astype(str)
-y = data['class']
+train, test = train_test_split(data, test_size=0.2)
+train_x = train['text'].astype(str)
+test_x = test['text'].astype(str)
+
+train_y = train['class']
+test_y = test['class']
 
 # Bag Of Words Model
 # (https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html)
 
 count_vectorizer = CountVectorizer(stop_words='english', min_df=0.0001, binary=True)
-x = count_vectorizer.fit_transform(texts)
-
-print(x.getrow(0))
+train_x = count_vectorizer.fit_transform(train_x)
+test_x = count_vectorizer.transform(test_x)
 
 print('Start training')
-model = LogisticModel(2, 10, 0.5)
+model = LogisticModel(0.001, 1000000, 0.5)
 # x is a 2d sparse array from scipy.sparse
 # x: [
 #   [00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001
 #
 #
 #     ]
-model.train(x, np.array(y))
+
+model.train(train_x, np.array(train_y))
+
+print(model.loss(test_x, np.array(test_y)))
 
 ip = " "
 while ip != "":
     ip = input("> ")
     x2 = count_vectorizer.transform([ip])
-    print(round(model.pred(x2.toarray()[0]), 10))
+    print(round(model.pred(x2.toarray()[0])[0][0], 10))
